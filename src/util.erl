f2e39f3949d9558f3a2e50340f9c087f9fa85323
Raghav Karol <raghav.karol@alertlogic.com>
2020-01-14 10:50:09 +0100

    Use a single directory for compiled beam files
    While it's manageable to figure out the ebin directory that rebar3
    will write source files to test files are e.g. ct suite files are
    problematic since rebar3 compies these files into the the _build
    directory. This causes a problem:
    
    ```
    rebar3 ct
    ```
    
    Now the suite files are copied to the _build directory, the watcher
    discovers that these files are changed we re-compile and add the path
    where the beam is written to the code path, which is different for the
    suite file managed by git. The consequence, watch and re-compile works
    but we reload the beam file generated by rebar3 instead of the one
    just re-compiled.
    
    Having a single directory for beam files generated by erl_dev_tools
    avoid all of these problems.

--- src/util.erl
+++ src/util.erl
@@ -104,74 +104,16 @@ compile(Path, Flags) ->
 
     compile:file(Path, Opts).
 
-is_test(Path) ->
-    [[]|Words] = string:split(Path, "/", all),
-    lists:member("test", Words).
-
-is_checkouts(Path) ->
-    [[]|Words] = string:split(Path, "/", all),
-    lists:member("_checkouts", Words).
-
-outdir(Path) ->
+outdir(_Path) ->
     Home = project_home(),
-    App = app_name(Path),
-    case is_test(Path) of
-        true ->
-            filename:dirname(Path);
-        false ->
-            case is_checkouts(Path) of
-                true ->
-                    Home ++ "/" ++ "_checkouts" ++ "/" ++ App ++ "/ebin/";
-                false ->
-                             Profile = profile_name(Path),
-                    Home ++ "/" ++ "_build" ++ "/" ++ Profile ++ "/lib/" ++ App ++ "/ebin/"
-            end
-    end.
-
+    Dir = Home ++ "/_build/erl_dev_tools_ebin/",
+    filelib:ensure_dir(Dir),
+    Dir.
 
 project_home() ->
     {ok, DefaultHome} = file:get_cwd(),
     DefaultHome1 = application:get_env(erl_dev_tools, project_home, DefaultHome),
     os:getenv("ERL_DEV_TOOLS_PROJECT_HOME", DefaultHome1).
-
-drop_until_word(W, [W|Rest]) ->
-    [W|Rest];
-drop_until_word(W, [_|Rest]) ->
-    drop_until_word(W, Rest);
-drop_until_word(_W, []) ->
-    [].
-
-app_name1([App, "src"|_]) ->
-    App;
-app_name1([App, "test"|_]) ->
-    App;
-app_name1([_, _|Rest]) ->
-    app_name1(Rest);
-app_name1([_]) ->
-    undefined;
-app_name1([]) ->
-    undefined.
-
-
-app_name(Path) ->
-    [[]|Words] = string:split(Path, "/", all),
-    app_name1(Words).
-
-profile_name(Path) ->
-    [[]|Words] = string:split(Path, "/", all),
-    Words1 = drop_until_word("_build", Words),
-    case Words1 of
-        [] ->
-            case lists:reverse(Words) of
-                [_Filename, "src"|_] ->
-                    "default";
-                [_Filename, "test"|_] ->
-                    "test"
-            end;
-        ["_build", RebarProfile|_] ->
-            RebarProfile
-    end.
-
 
 %% [{"/Users/raghav/github/erl_dev_tools/test_data/compile_fail.erl",
 %%           [{5,erl_parse,["syntax error before: ",[]]}]}]
@@ -238,65 +180,6 @@ is_in_src_dir(Path) ->
     end.
 
 -ifdef(TEST).
-app_name_test() ->
-    ?assertEqual(
-       "aefr_eng",
-       app_name("/Users/rkarol-admin/github/alertlogic/aefr_eng/src/aefr_eng.erl")),
-
-    ?assertEqual(
-       "aefr_eng",
-       app_name("/Users/rkarol-admin/github/alertlogic/aefr_eng/test/aefr_eng_suite.erl")),
-    ok.
-
-profile_name_test() ->
-    ?assertEqual(
-       "test",
-       profile_name("/Users/rkarol-admin/github/alertlogic/aefr_eng/test/aefr_eng_SUITE.erl")),
-
-    ?assertEqual(
-       "default",
-       profile_name("/Users/rkarol-admin/github/alertlogic/aefr_eng/src/aefr_eng.erl")),
-    ?assertEqual(
-       "default",
-       profile_name("/Users/rkarol-admin/github/alertlogic/aefr_eng/_build/default/lib/al_sd2/src/al_sd2.erl")),
-
-    ?assertEqual(
-       "eqc",
-       profile_name("/Users/rkarol-admin/github/alertlogic/aefr_eng/_build/eqc/lib/al_sd2/src/al_sd2.erl")),
-    ?assertEqual(
-       "test",
-       profile_name("/Users/rkarol-admin/github/alertlogic/aefr_eng/_build/test/lib/meck/src/meck.erl")),
-    ok.
-
-outdir_test() ->
-    ?assertEqual(
-       "/Users/rkarol-admin/github/alertlogic/aefr_eng/_build/default/lib/aefr_eng/ebin/",
-       outdir("/Users/rkarol-admin/github/alertlogic/aefr_eng/src/aefr_eng.erl")),
-
-    ?assertEqual(
-       "/Users/rkarol-admin/github/alertlogic/aefr_eng/_build/test/lib/aefr_eng/ebin/",
-       outdir("/Users/rkarol-admin/github/alertlogic/aefr_eng/test/aefr_eng_SUITE.erl")),
-
-    ?assertEqual(
-       "/Users/rkarol-admin/github/alertlogic/aefr_eng/_build/default/lib/al_sd2/ebin/",
-       outdir("/Users/rkarol-admin/github/alertlogic/aefr_eng/_build/default/lib/al_sd2/src/al_sd2.erl")),
-    ok.
-
-%% compile_opts_rebar3_test() ->
-%%     application:set_env(erl_dev_tools, mode, rebar3),
-%%     {ok, Cwd} = file:get_cwd(),
-%%     [ErlSrcFile|_] = filelib:wildcard(Cwd ++ "/src/*.erl"),
-%%     [ErlTestFile|_] = filelib:wildcard(Cwd ++ "/test/*.erl"),
-%%     ExpectedOutDir = Cwd ++ "/_build/erl_dev_tools/lib/erl_dev_tools/ebin/",
-%%     ExpectedOutDir = compile_opts(outdir, ErlSrcFile),
-%%     ExpectedOutDir = compile_opts(outdir, ErlTestFile),
-
-%%     ExpectedIncludeDirs = [Cwd ++ "/_build/default/lib",
-%%                            Cwd ++ "/include",
-%%                            Cwd ++ "/_build/default/lib/erl_dev_tools/include"],
-%%     ExpectedIncludeDirs = compile_opts(i, ErlSrcFile),
-%%     ok.
-
 is_ct_suite_test() ->
     false = is_ct_suite("/a/b/c/x.erl"),
     false = is_ct_suite("/a/b/c/x_SUITE.beam"),
